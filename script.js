6bea4bf842470cc0630b0285649b40d041d8e1a4
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-10-07 20:57:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function nextPalindrome(num) {
   //your JS code here.
+	 const numStr = num.toString();
+
+  // Reverse the string and check if it's the same as the original
+  return numStr === numStr.split('').reverse().join('');
+}
+
+function nextPalindrome(input) {
+  let num = input + 1;
+
+  while (true) {
+    if (isPalindrome(num)) {
+      alert(num);
+      return;
+    }
+    num++;
+  }
 }
 
 const input = prompt("Enter a palindrome number");
